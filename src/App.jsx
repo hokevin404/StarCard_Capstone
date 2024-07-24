c5c39481950cefb5ec7604b2dc8572f606a75533
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-24 15:53:54 -0700

    Connected Navbar comp to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react'
+import './App.css';
+import NavBar from './components/NavBar/NavBar.jsx';;
 
 function App() {
 
   return (
     <>
-      
+      <NavBar />
     </>
   )
-}
+};
 
 export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 function NavBar() {
   return (
     <div>
-        
+        <h1>NavBar</h1>
     </div>
   )
 }
