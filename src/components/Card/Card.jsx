f3aa8aefbbdcfbce52db9300825dfb0248cf5f16
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-24 21:11:42 -0700

    Added Card comp

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Card() {
+    return (
+        <div className="card">
+            <img src="src/assets/200_img_placeholder.png" alt="Avatar" />
+            <div className="container">
+                <p>Dreamcatcher Justice</p>
+                <h3><b>$50.99</b></h3>
+            </div>
+        </div>
+    )
+}
+
+export default Card
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,16 +1,11 @@
-import React from 'react'
+import Card from '../Card/Card';
+import React from 'react';
 
 function Shop() {
     return (
         <>
             <div className='cardContainer'>
-                <div className="card">
-                    <img src="src/assets/200_img_placeholder.png" alt="Avatar" />
-                    <div className="container">
-                        <p>Dreamcatcher Justice</p>
-                        <h3><b>$50.99</b></h3>
-                    </div>
-                </div>
+                <Card />
             </div>
         </>
 
