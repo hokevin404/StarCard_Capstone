ebcaee3d35b64f6ccc812f3179369b2bc9867dff
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-24 20:13:04 -0700

    Banner css

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Banner() {
   return (
-    <div>
+    <div className='banner'>
         <img src='src/assets/Kpop-banner.png' alt='Kpop Banner' />
     </div>
   )
