9f11abdf26d79d10400444ff36e42807601d41f5
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-24 21:03:04 -0700

    Added card placeholder

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 function Shop() {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <>
+            <div className='cardContainer'>
+                <div className="card">
+                    <img src="src/assets/200_img_placeholder.png" alt="Avatar" />
+                    <div className="container">
+                        <p>Dreamcatcher Justice</p>
+                        <h3><b>$50.99</b></h3>
+                    </div>
+                </div>
+            </div>
+        </>
+
+    )
 }
 
 export default Shop
