201903b8bb0872c6c5e64e1c9693e60a27debcc8
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-24 20:09:53 -0700

    Added Banner img

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import './App.css';
 import NavBar from './components/NavBar/NavBar.jsx';
+import Banner from './components/Banner/Banner.jsx';
 
 function App() {
 
   return (
     <>
       <NavBar />
+      <Banner />
     </>
   )
 };
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,10 @@
+import './banner.css';
 import React from 'react'
 
 function Banner() {
   return (
     <div>
-        
+        <img src='src/assets/Kpop-banner.png' alt='Kpop Banner' />
     </div>
   )
 }
