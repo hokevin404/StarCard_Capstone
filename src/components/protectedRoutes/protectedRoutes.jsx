d222ce7b6beb0cdfe5c6c70de8465ab6b31ba33d
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-27 23:21:19 -0700

    Completed protectedRoutes comp

--- src/components/protectedRoutes/protectedRoutes.jsx
+++ src/components/protectedRoutes/protectedRoutes.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom';
+import { useAuth } from 'src/context/auth/authContext';
 
 function protectedRoutes() {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+    const { cookies } = useAuth();
+
+    return cookies.token ? <Outlet/> : <h1> You are not authorized to view</h1>
+};
 
 export default protectedRoutes
