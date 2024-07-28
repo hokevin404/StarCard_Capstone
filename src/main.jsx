df2cc0a3dd3072fd545cbac0e8784a27583b39d9
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-27 23:08:15 -0700

    Import AppProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import AppProvider from './context/appContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <AppProvider>
+      <Router>
+        <App />
+      </Router>
+    </AppProvider>
   </React.StrictMode>,
 )
