35fa57e229282d1ba89d126b72bd278aa39b3f01
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-27 19:27:48 -0700

    Added SignUp comp

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -2,7 +2,11 @@
 function SignUp() {
   return (
     <div className="signupContainer">
-        
+        <h3>Sign Up</h3>
+        <form autoComplete="off">
+          <label htmlFor="First Name">First Name</label>
+          <input type="text" id="fname" name="fname" placeholder="first name" />
+        </form>
     </div>
   )
 }
