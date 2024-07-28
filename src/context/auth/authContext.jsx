317e3744ed6cb35375fe0dc6177a2b1630897c8b
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-27 21:50:19 -0700

    Added UserProvider boilerplate

--- src/context/auth/authContext.jsx
+++ src/context/auth/authContext.jsx
@@ -1,3 +1,13 @@
 import { createContext, useContext, useMemo } from "react";
 import { useCookies } from "react-cookie";
 import axios from "axios";
+
+const AuthContext = createContext(
+
+);
+
+export function UserProvider({children}) {
+    
+
+    return <AuthContext.Provider>{children}</AuthContext.Provider>
+};
