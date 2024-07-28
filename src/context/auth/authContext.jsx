2ab8d95a77b1d1d977c7ec5161b156680167794e
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-27 22:19:59 -0700

    Added login/ signup boilerplate function

--- src/context/auth/authContext.jsx
+++ src/context/auth/authContext.jsx
@@ -10,5 +10,23 @@ export function UserProvider({children}) {
     // Create cookies
     const [cookies, setCookies, removeCookies] = useCookies();
 
+    // Login Function
+    const login = async (formData) => {
+        try {
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    // SignUp Function
+    const signUp = async (formData) => {
+        try {
+            
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return <AuthContext.Provider>{children}</AuthContext.Provider>
 };
